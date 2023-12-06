6b60c6b8b7db884f106f12409d3baf1f95fb5baa
kt <kt@ciphron.de>
2023-12-06 14:00:15 +0100

    remove service.uri

--- run-bootable.sh
+++ run-bootable.sh
@@ -8,5 +8,5 @@ export POSTGRESQL_PASSWORD=trust
 
 mvn clean package
 sleep 5
-java -jar -Dcustomer.service.uri=http://localhost:8083/ -Dstore.service.uri=http://localhost:8082/ -Dfilm.service.uri=http://localhost:8081/ -Djboss.http.port=8083 -Djboss.management.http.port=9993 target/starter-bootable.jar
+java -jar -Djboss.http.port=8083 -Djboss.management.http.port=9993 target/starter-bootable.jar
 
