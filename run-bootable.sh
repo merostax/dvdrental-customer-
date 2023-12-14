8d9820023f54953c7f780e4b71e76bab768e250e
kt <kt@ciphron.de>
2023-12-14 17:38:39 +0100

    edit persistence.xml

--- run-bootable.sh
+++ run-bootable.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-export POSTGRESQL_SERVICE_HOST=localhost
-export POSTGRESQL_SERVICE_PORT=54323
-export POSTGRESQL_USER=postgres
-export POSTGRESQL_PASSWORD=trust
-
-
 mvn clean package
 sleep 5
-java -jar -Dcustomer.service.uri=http://localhost:8083/ -Dstore.service.uri=http://localhost:8082/ -Dfilm.service.uri=http://localhost:8081/ -Djboss.http.port=8083 -Djboss.management.http.port=9993 target/starter-bootable.jar
+java -jar -Ddvdrental.jdbc.url=jdbc:postgresql://localhost:5432/dvdrentalcustomer -Dcustomer.service.uri=http://localhost:8083/ -Dstore.service.uri=http://localhost:8082/ -Dfilm.service.uri=http://localhost:8081/ -Djboss.http.port=8083 -Djboss.management.http.port=9993 target/starter-bootable.jar
 
